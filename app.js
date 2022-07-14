09461d88a6bf1a0a6d52cf840368a156041b97a7
JiokPanda <jjhjjhbr@gmail.com>
2022-07-15 02:07:16 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const p1 = {
+  score: 0,
+  button: document.querySelector("#p1Button"),
+  display: document.querySelector("#p1Display"),
+};
+
+const p2 = {
+  score: 0,
+  button: document.querySelector("#p2Button"),
+  display: document.querySelector("#p2Display"),
+};
+
+const resetButton = document.querySelector("#reset");
+const winningScoreSelect = document.querySelector("#playto");
+
+let winningScore = 3;
+let isGameOver = false;
+
+function score(player, opposition) {
+  if (!isGameOver) {
+    player.score += 1;
+    if (player.score === winningScore) {
+      isGameOver = true;
+      player.display.classList.add("has-text-success");
+      opposition.display.classList.add("has-text-danger");
+      player.button.disabled = true;
+      opposition.button.disabled = true;
+    }
+    player.display.textContent = player.score;
+  }
+}
+
+p1.button.addEventListener("click", function () {
+  score(p1, p2);
+});
+
+p2.button.addEventListener("click", function () {
+  score(p2, p1);
+});
+
+resetButton.addEventListener("click", reset);
+
+winningScoreSelect.addEventListener("change", function () {
+  winningScore = parseInt(this.value);
+  reset();
+});
+
+function reset() {
+  isGameOver = false;
+  p1.score = 0;
+  p2.score = 0;
+  p1.display.textContent = p1.score;
+  p2.display.textContent = p2.score;
+  p1.display.classList.remove("has-text-success", "has-text-danger");
+  p2.display.classList.remove("has-text-success", "has-text-danger");
+  p1.button.disabled = false;
+  p2.button.disabled = false;
+}
